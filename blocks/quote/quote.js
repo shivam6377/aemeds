b2fcebe10fb246536da427f0774783d8ec34f686
shivam6377 <bajajshivam444@gmail.com>
2024-06-27 17:40:35 +0530

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -2,11 +2,11 @@ export default function decorate(block) {
     const [quoteWrapper, quoteWrapper2] = block.children;
   
     const blockquote = document.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
+    blockquote.textContent = "hello";
     quoteWrapper.replaceChildren(blockquote);
 
     const blockquote2 = document.createElement('blockquote');
-    blockquote2.textContent = quoteWrapper2.textContent.trim();
+    blockquote2.textContent = "Hi";
     quoteWrapper2.replaceChildren(blockquote2);
   }
   
