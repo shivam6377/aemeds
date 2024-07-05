71f4d979d328b585496123315881bdbcf3ef9f52
shivam6377 <bajajshivam444@gmail.com>
2024-07-05 18:28:17 +0530

    Update secondary-navigation.js

--- blocks/secondary-navigation/secondary-navigation.js
+++ blocks/secondary-navigation/secondary-navigation.js
@@ -1,28 +1,33 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
- 
     const [imageSection, ...ctaSections] = block.children;
 
+    // Get image source and alt text
     const imageSrc = imageSection.querySelector('img')?.src;
     const imageAlt = imageSection.querySelector('img')?.alt || 'navbar';
 
-    const ctaElements = ctaSections.map(section => {
-        const linkElement = section.querySelector('.button-container a');
-        const link = linkElement?.href || '#';
-        const linkText = linkElement?.textContent?.trim() || '';
-        
+    // Generate cta elements
+    const ctaElements = ctaSections
+        .filter(section => section.querySelector('.button-container a')) // Filter out sections without .button-container a
+        .map(section => {
+            const linkElement = section.querySelector('.button-container a');
+            const link = linkElement.href || '#';
+            const linkText = linkElement.textContent?.trim() || '';
+            const linkTitle = linkElement.title || '';
 
-        return `
-            <li>
-                <a href="${link}" title="${linkTitle}" class="button">${linkText}</a>
-            </li>
-        `;
-    }).join('');
+            return `
+                <li>
+                    <a href="${link}" title="${linkTitle}" class="button">${linkText}</a>
+                </li>
+            `;
+        })
+        .join('');
 
+    // Construct the navbar HTML
     block.innerHTML = `
-    <nav class="navbar">
-    <div class="logo-container">
+        <nav class="navbar">
+            <div class="logo-container">
                 <img src="${imageSrc}" alt="${imageAlt}"/>
             </div>
             <div class="buttons-container">
@@ -32,6 +37,4 @@ export default function decorate(block) {
             </div>
         </nav>
     `;
-    
-
 }
