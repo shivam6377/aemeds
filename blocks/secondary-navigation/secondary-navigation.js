75040339bf4b8c4802cb1f783fac383651d3bd95
shivam6377 <bajajshivam444@gmail.com>
2024-07-05 17:51:19 +0530

    Update secondary-navigation.js

--- blocks/secondary-navigation/secondary-navigation.js
+++ blocks/secondary-navigation/secondary-navigation.js
@@ -1,22 +1,22 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-    const block = block.querySelector('.secondary-navigation.block');
+   
     const [imageSection, ...ctaSections] = block.children;
 
     const imageSrc = imageSection.querySelector('img')?.src;
     const imageAlt = imageSection.querySelector('img')?.alt || 'Widget';
 
     const ctaElements = ctaSections.map(section => {
-        const linkElement = section.querySelector('.button-container a');
+        const linkElement = section.querySelector('a');
         const link = linkElement?.href || '#';
         const linkText = linkElement?.textContent?.trim() || '';
         const linkTitle = linkElement?.title || '';
 
         return `
-            <li>
+        
                 <a href="${link}" title="${linkTitle}" class="button">${linkText}</a>
-            </li>
+         
         `;
     }).join('');
 
@@ -26,9 +26,9 @@ export default function decorate(block) {
                 <img src="${imageSrc}" alt="${imageAlt}"/>
             </div>
             <div class="buttons-container">
-                <ul>
+            
                     ${ctaElements}
-                </ul>
+              
             </div>
         </nav>
     `;
