072e38228b2337dcfeba688e2e9c4573fc36e6c2
shivam6377 <bajajshivam444@gmail.com>
2024-07-06 11:42:34 +0530

    Update secondary-navigation.js

--- blocks/secondary-navigation/secondary-navigation.js
+++ blocks/secondary-navigation/secondary-navigation.js
@@ -1,34 +1,38 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
     const [imageEl, imageAltTextEl, ...ctasEl] = block.children;
     const imageSrc = imageEl?.querySelector('img')?.src;
     const alt = imageAltTextEl?.querySelector('img')?.alt || 'navbar';
-
     const ctaElements = ctasEl.map((element) => {
-        const [ctaTextEl, linkEl] = element.children;
+        const [ ctaTextEl, linkEl] = element.children;
+        
         const ctaText = ctaTextEl?.textContent?.trim() || '';
         const link = linkEl?.querySelector('.button-container a')?.href;
-        const isActive = linkEl?.querySelector('.button-container a')?.classList.contains('active') ? 'active' : '';
+        
 
-        return `
-            <a href="${link}" class="nav-button ${isActive}">
+        element.innerHTML = `
+       
+            <a href="${link}" class="nav-button ">
+               
                 <p>${ctaText}</p>
-            </a>
+       
         `;
+        moveInstrumentation(element, element.firstElementChild);
+        return element.innerHTML;
     }).join('');
 
     block.innerHTML = `
     <nav class="navbar">
-        <div class="logo-container">
+    <div class="logo-container">
             <img src="${imageSrc}" alt="${alt}">
+         
         </div>
         <div class="buttons-container">
-            ${ctaElements}
+    
+                ${ctaElements}
+          
         </div>
-    </nav>
-    `;
-
-    // Move instrumentation for the updated structure
-    moveInstrumentation(block, block.querySelector('.navbar'));
+        </nav>
+        `;
+ 
 }
