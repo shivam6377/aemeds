c7ec3257887f1d0694c8c1b8f5a3f7ebc85e48df
shivam6377 <bajajshivam444@gmail.com>
2024-07-08 11:13:00 +0530

    Update secondary-navigation.js

--- blocks/secondary-navigation/secondary-navigation.js
+++ blocks/secondary-navigation/secondary-navigation.js
@@ -1,9 +1,8 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-    const [imageEl, imageAltTextEl,imageClickedEl, ...ctasEl] = block.children;
+    const [imageEl, imageAltTextEl, ...ctasEl] = block.children;
     const imageSrc = imageEl?.querySelector('img')?.src;
-    const imageClickedSrc = imageClickedEl?.querySelector('img')?.src;
     const alt = imageAltTextEl?.querySelector('img')?.alt || 'navbar';
 
     const ctaElements = ctasEl.map((element, index) => {
