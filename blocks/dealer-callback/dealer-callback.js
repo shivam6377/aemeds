023cec8f13a8225ed0f2d6fb64809aa30bc56286
shivam6377 <bajajshivam444@gmail.com>
2024-07-16 13:27:17 +0530

    js changes for dealer

--- blocks/dealer-callback/dealer-callback.js
+++ blocks/dealer-callback/dealer-callback.js
@@ -1,13 +1,12 @@
 import utility from '../../utility/utility.js';
 import teaser from '../../utility/teaserUtils.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const [...teaserListEl] = block.children;
 
   const teasers = teaserListEl.map((card) => {
     const teaserObj = teaser.getTeaser(card)?.firstElementChild;
-    moveInstrumentation(card, teaserObj);
+    // moveInstrumentation(card, teaserObj); // Removed the moveInstrumentation functionality
     utility.mobileLazyLoading(teaserObj, '.teaser__image img');
     return teaserObj.outerHTML;
   });
